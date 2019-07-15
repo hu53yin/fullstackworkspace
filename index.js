f5aa0b67dca1ff84294b4cae9ad931ff82ce4076
hu53yin <huseyingurkan@gmail.com>
2019-07-16 01:27:55 +0300

    mongoose settings was added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require("express");
-require("./services/passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
-require("./routes/authRoutes")(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
