7224f23f85368539faf70b10e784b6b12a304918
Huseyin Gurkan <huseyingurkan@gmail.com>
2019-07-18 17:41:54 +0300

    client project was added

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hi there!</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="/auth/google">Sign In With Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
