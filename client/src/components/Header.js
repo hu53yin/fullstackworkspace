0b5c634e558b331ff4067f0b02ff31906eeb6de1
hu53yin <huseyingurkan@gmail.com>
2019-07-22 02:15:51 +0300

    client side development was completed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent() {
+    console.log(this.props.auth);
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
-        <div class="nav-wrapper">
-          <a class="left brand-logo">Emaily</a>
-          <ul class="right">
-            <li>
-              <a href="sass.html">Login With Google</a>
-            </li>
-          </ul>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
