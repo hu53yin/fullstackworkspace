750604f761aba8791d525af528055c2ce573d36b
hu53yin <huseyingurkan@gmail.com>
2019-07-25 07:56:01 +0300

    update dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="fixed-action-btn">
+      <a className="btn-floating btn-large red">
+        <i className="material-icons">add</i>
+      </a>
+    </div>
+  );
 };
 
 export default Dashboard;
