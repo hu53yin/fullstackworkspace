32e2fd6a0d9ed4425ea96ed0ccea9e9576a20175
hu53yin <huseyingurkan@gmail.com>
2019-07-29 02:04:10 +0300

    surveyTemplate development

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,29 +1,51 @@
-const mongoose = require("mongoose");
-const requireLogin = require("../middlewares/requireLogin");
-const requireCredits = require("../middlewares/requireCredits");
-const Mailer = require("../services/Mailer");
-const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
-const Survey = mongoose.model("surveys");
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get("/api/surveys/thanks", (req, res) => {
-    res.send("Thanks for voting!");
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!');
   });
 
-  app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice
+          };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
+
+    console.log(events);
+
     res.send({});
   });
 
-  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
     });
